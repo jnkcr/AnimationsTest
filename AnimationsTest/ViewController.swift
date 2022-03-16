225a84f72cdd10705e98a9b29932e96235705cb2
Jan Kučera <kucera.jan@hotmail.cz>
2022-03-16 10:14:39 +0100

    Initial Commit

---
+++ AnimationsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationsTest
+//
+//  Created by Jan Kučera on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
